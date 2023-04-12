fc125c369c433c617a8527aa0f5fa8da44e86c44
huaydin <huaydin@student.42vienna.com>
2023-04-12 18:51:48 +0200

    a

--- pstester.py
+++ pstester.py
@@ -28,14 +28,14 @@ if not os.path.exists(checker_filename) or not os.access("./"+checker_filename, 
 
 
 def testcase(nbrs):
-    result = subprocess.run(["valgrind", "--leak-check=full", "./push_swap", nbrs], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run(["timeout", "10", "./push_swap", nbrs], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(f"{yellow}[Numbers]:{reset} {nbrs.ljust(40)}", end="")
     output = result.stderr.decode()
     memory_usage = re.search(r"in use at exit: (\d+) bytes in", output)
     memory_errors = re.search(r"ERROR SUMMARY: (\d+) errors", output)
-    num_inuse = 0
+    num_inuse = None  # Initialize to None instead of 0
     if memory_usage:
-        num_inuse = int(memory_usage.group(1))
+        num_inuse = memory_usage.group(1)  # Assign the matched string to num_inuse
     num_memerr = 0
     if memory_errors:
         num_memerr = int(memory_errors.group(1))
@@ -48,7 +48,7 @@ def testcase(nbrs):
         res2=f"{green}OK{reset}"
     if re.search(r"KO", output2):
         res2=f"{red}KO{reset}"
-    if num_inuse:
+    if num_inuse is not None:  # Check if num_inuse is not None before calling group()
         print(f"{red}MKO{reset} {num_inuse} bytes still reachable!".ljust(40), end="")
     elif num_memerr:
         print(f"{red}MKO{reset} {num_memerr} memory errors!".ljust(40), end="")
@@ -60,8 +60,9 @@ def testcase(nbrs):
         print(f"Error handling: {red}KO{reset}".ljust(30), end="")
     if not re.search(r"Error\n", output2):
         print(f"Sorting: {res2}".ljust(15), end="")
-    
     print("")
+
+
 
 
 testcase("")
@@ -81,7 +82,7 @@ testcase("2147483648 -2147483649")
 testcase("0 1 2 3 4 5 6 7 8 9")
 testcase("9 8 7 6 5 4 3 2 1 0")
 testcase("5 3 1 2 4 6")
-testcase("10000000000 20000000000 30000000000")
+testcase("2147483649 2147483649 2147483649")
 testcase("2 2 2 2 2 2 2")
 testcase("10 9 8 7 6 5 4 3 2 1 0")
 
